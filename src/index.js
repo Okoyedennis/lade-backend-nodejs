4a00f6109bc108f74506de10da4282f85270f0b2
Dennis <okoyedennis7@gmail.com>
2023-07-08 08:15:14 +0100

    added cors to project

--- src/index.js
+++ src/index.js
@@ -2,17 +2,22 @@ const express = require("express");
 require("./db/mongoose");
 const userRouter = require("./router/user");
 const applyRouter = require("./router/apply");
-const donationRouter = require("./router/donation")
-const cors = require("cors")
+const donationRouter = require("./router/donation");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
-app.use(cors())
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://lade-frontend.vercel.app"],
+    credentials: true,
+  })
+);
 app.use(userRouter);
-app.use(applyRouter); 
-app.use(donationRouter); 
+app.use(applyRouter);
+app.use(donationRouter);
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
